f4181a0819e0f619ae8c281e6055c1d215939bb4
Carlos-Marte <carlosmartedev@hotmail.com>
2023-01-26 18:41:10 -0400

    Agregando script

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,4 @@
-const url = new URL("https://weatherapi-com.p.rapidapi.com/current.json?");
-url.searchParams.append("q", "República Dominicana");
-url.searchParams.append("lang", "es");
-
+const url = new URL("https://weatherapi-com.p.rapidapi.com/current.json?lang=es");
 const options = {
 	method: 'GET',
 	headers: {
@@ -10,7 +7,53 @@ const options = {
 	}
 };
 
-fetch(url, options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+const iconButton = document.querySelector("#icon-button");
+
+const checkInput = () => {
+	const inputCountry = document.querySelector("#input-country");
+	const inputValue = inputCountry.value.trim();
+
+	if (inputValue !== "") {
+		inputCountry.value = "";
+		return inputValue;
+	}
+
+	alert("ERROR: Debes colocar un país.");
+}
+
+
+const getWeather = async (country) => {
+	url.searchParams.set("q", country);
+
+	try {
+		const response = await fetch(url, options);
+		const data = await response.json();
+		console.log(data);
+
+		const containerInformation = document.querySelector("#information-container");
+		containerInformation.innerHTML = `
+			<div class="information__text information__text--l information__text--country">${data.location.country}, ${data.location.name}</div>
+			<div class="information__text information__text--xl information__text--temperature">${data.current.temp_c}°C</div>
+			<div class="information__container information__container--condition">
+				<img src="${data.current.condition.icon}" alt="Time condition icon" loading="lazy" class="information__icon">
+				<div class="information__text information__text--l">${data.current.condition.text}</div>
+			</div>
+			<div class="information__text information__text--sensation">Sensación: ${data.current.feelslike_c}°C</div>
+			<div class="information__text information__text--wind">Viento: ${data.current.wind_kph} / kph</div>
+			<div class="information__text information__text--humidity">Humedad: ${data.current.humidity}%</div>`;
+
+	} catch (error) {
+		alert("ERROR: Ha ocurrido un error al obtener la información.");
+		console.error(error);
+	}
+}
+
+const placeData = async () => {
+	const country = checkInput();
+
+	if (country !== undefined) {
+		getWeather(country);
+	}
+}
+
+iconButton.addEventListener('click', placeData);
